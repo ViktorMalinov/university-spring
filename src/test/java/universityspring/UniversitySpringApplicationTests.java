50eba74f4958ca1f45dae0902981b910d3985206
Viktor Malinov <v.malinov@kodar.net>
2021-06-28 14:02:58 +0300

    first commit - 28.06.2021

---
+++ src/test/java/universityspring/UniversitySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package universityspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
