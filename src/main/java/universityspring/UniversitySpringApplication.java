9c21e2b9370f8aa37fe492083ded2bfb8b9fa3e3
Viktor Malinov <v.malinov@kodar.net>
2021-07-13 11:25:05 +0300

    test

--- src/main/java/universityspring/UniversitySpringApplication.java
+++ src/main/java/universityspring/UniversitySpringApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
+//@EntityScan(value = {"main.dataaccess"})
+
 @SpringBootApplication
-@EntityScan(value = {"main.dataaccess"})
+@EntityScan(value = {"main"})
 @ComponentScan(value = {"main"})
 public class UniversitySpringApplication {
 
