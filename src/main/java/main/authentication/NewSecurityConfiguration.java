32dc14351345a92f1014b37a3d86de8aa1412b19
Viktor Malinov <v.malinov@kodar.net>
2021-10-01 17:53:15 +0300

    working on login page

--- src/main/java/main/authentication/NewSecurityConfiguration.java
+++ src/main/java/main/authentication/NewSecurityConfiguration.java
@@ -99,7 +99,8 @@ public class NewSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .defaultSuccessUrl("http://www.google.com", true)
+                .defaultSuccessUrl("http://www.google.com", false)
+                //.defaultSuccessUrl("/homepage", true)
                 .failureUrl("http://www.yahoo.com")
         ;
     }
